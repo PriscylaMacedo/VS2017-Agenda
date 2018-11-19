7933d25b36f6b66f487a5e805ce2c192e757ac8c
Priscyla Macedo <priscyla.macedo@gmail.com>
2018-11-19 13:40:45 -0200

    Primeiro Commit

---
+++ AgendaTestePratico/AgendaTestePratico/Models/pessoa.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace AgendaTestePratico.Models
+{
+    public class pessoa
+    {
+        [Key]
+        public int Codigo { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public string Cidade { get; set; }
+        public string Email { get; set; }
+        public string Telefone { get; set; }
+        public DateTime Data_Nascimento { get; set; }
+
+    }
+}
